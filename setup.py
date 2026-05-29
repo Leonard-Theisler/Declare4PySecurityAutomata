cbf9be043fa91b015d2a45e7c8cb70cb9f0d1a15
Matteo <mazorzi@unibz.it>
2026-05-29 15:29:18 +0200

    changed version to 2.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name="declare4py",
-    version='{{VERSION_PLACEHOLDER}}',
+    version='2.2.1',
     author="Ivan Donadello, Fabrizio Maria Maggi, Francesco Riva",
     author_email="donadelloivan@gmail.com, maggi@inf.unibz.it, Francesco.Riva@unibz.it",
     description="Python library to perform discovery, conformance checking and query checking of DECLARE constraints.",
