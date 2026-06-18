7d0bd71adf98580b546cba0fe7cc12a15a4023fc
leona <s204688@dtu.dk>
2026-06-18 17:00:15 +0200

    refactored factory to use switch case. implemented more patterns

--- Declare4Py/SecurityAutomata/SecurityAutomataFactory.py
+++ Declare4Py/SecurityAutomata/SecurityAutomataFactory.py
@@ -36,22 +36,54 @@ class SecurityAutomataFactory:
         return declare_constraints
               
     def generateAutomatonFromTemplate(self, template: str, actions: List[str]):
-        if template == "Chain Response":
-            states = ["Q0", "Q1", "Q2"] 
-            transitions = {("!"+actions[0], "Q0"): "Q0", (actions[0], "Q0"): "Q1", (actions[1], "Q2"): "Q0"}
-            suppressions = {("!"+actions[0], "+"): "Q0", (actions[0], "Q0"): "-"}
-            insertions = {("!"+actions[1], "Q1"): ([actions[0], actions[1]], "Q0"), (actions[1], "Q1"): ([actions[0]], "Q2")}
-            return EditAutomaton("Q0", states, transitions, suppressions, insertions)
-        elif template == "Chain Precedence":
-            states = ["Q0", "Q1"]
-            transitions = {(actions[1], "Q0"): "Q0", ("!"+actions[1], "Q0"): "Q1", (actions[1], "Q1"): "Q0"}
-            insertions = {(actions[0], "Q1"): ([actions[1]], "Q0")}
-            return InsertionAutomaton("Q0", states, transitions, insertions)
+        match template:
+            case "At Least One": #TODO implement buffer case in insert/edit automaton
+                states = ["Q0", "Q1", "Q2"]
+                transitions = {("!"+actions[0], "Q0"): "Q0", (actions[0], "Q1"): "Q2"}
+                suppressions = {("!"+actions[0], "Q0"): "-", (actions[0], "Q1"): "+"}
+                insertions = {(actions[0], "Q0"): (["buffer"], "Q1")}
+                return EditAutomaton("Q0", states, transitions, suppressions, insertions)
+            case "At Most One":
+                states = ["Q0", "Q1"]
+                transitions = {("!"+actions[0], "Q0"): "Q0", (actions[0], "Q0"): "Q1", ("!"+actions[0], "Q1"): "Q1"}
+                return TruncationAutomaton("Q0", states, transitions)
+            case "Init":
+                states = ["Q0", "Q1"]
+                transitions = {(actions[0], "Q0"): "Q1"}
+                insertions = {("!"+actions[0], "Q0"): ([actions[0]], "Q1")}
+                return InsertionAutomaton("Q0", states, transitions, insertions)
+            case "End": #buffer case
+                states = ["Q0", "Q1", "Q2"]
+                transitions = {(actions[0], "Q0"): "Q0", ("!"+actions[0], "Q0"): "Q1", (actions[0], "Q2"): "Q0", (actions[0], "Q1"): "Q1"}
+                suppressions = {(actions[0], "Q0"): "+", ("!"+actions[0], "Q0"): "-", (actions[0], "Q2"): "+", (actions[0], "Q1"): "-"}
+                insertions = {("!"+actions[0], "Q1"): (["buffer"], "Q2")}
+                return EditAutomaton("Q0", states, transitions, suppressions, insertions)
+            case "Responded Existence":
+                states = ["Q0", "Q1", "Q2", "Q3"]
+                transitions = {(actions[0], "Q0"): "Q1", ("!"+actions[1], "Q1"): "Q1", (actions[1], "Q2"): "Q3", ("true", "Q3"): "Q3", (actions[1], "Q0"): "Q3"}
+                suppressions = {(actions[0], "Q0"): "-", ("!"+actions[1], "Q1"): "-", (actions[1], "Q2"): "+", ("true", "Q3"): "+", (actions[1], "Q0"): "+"}
+                insertions ={(actions[1], "Q1"): (["buffer"], "Q2")}
+                return EditAutomaton("Q0", states, transitions, suppressions, insertions)
+            case "Chain Response":
+                states = ["Q0", "Q1", "Q2"] 
+                transitions = {("!"+actions[0], "Q0"): "Q0", (actions[0], "Q0"): "Q1", (actions[1], "Q2"): "Q0"}
+                suppressions = {("!"+actions[0], "+"): "Q0", (actions[0], "Q0"): "-"}
+                insertions = {("!"+actions[1], "Q1"): ([actions[0], actions[1]], "Q0"), (actions[1], "Q1"): ([actions[0]], "Q2")}
+                return EditAutomaton("Q0", states, transitions, suppressions, insertions)
+            case "Chain Precedence":
+                states = ["Q0", "Q1"]
+                transitions = {(actions[1], "Q0"): "Q0", ("!"+actions[1], "Q0"): "Q1", (actions[1], "Q1"): "Q0"}
+                insertions = {(actions[0], "Q1"): ([actions[1]], "Q0")}
+                return InsertionAutomaton("Q0", states, transitions, insertions)
+            
         
 factory = SecurityAutomataFactory()
-model_path = os.path.join(os.path.dirname(__file__), "data-model2.decl")
-automata = factory.generateAutomataFromFile(model_path)
+# model_path = os.path.join(os.path.dirname(__file__), "data-model2.decl")
+# automata = factory.generateAutomataFromFile(model_path)
 
 
-for label, automaton in automata.items():
-    automaton.visualizeAutomaton(label)
+# for label, automaton in automata.items():
+#     automaton.visualizeAutomaton(label)
+
+aut = factory.generateAutomatonFromTemplate("Responded Existence", ["A", "B"])
+aut.visualizeAutomaton("Responded Existence A B")
